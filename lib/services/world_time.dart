ee6f7d0f107075b68758358416538b4e4fcef86f
Abraham Ortega <Abraham12orca@gmail.com>
2022-06-08 12:32:33 +0200

    isDayTime var
    Added a variable to know if the current time has sunlight or not

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -8,6 +8,7 @@ class WorldTime {
   late String time; // the time in that location
   late String flag; // url to an asset flag icon
   late String url; // this is the location url for api endpoint
+  late bool isDayTime; // true or false if daytime or not
 
   WorldTime({ required this.location, required this.flag, required this.url} );
 
@@ -37,6 +38,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
 
       // set the time property
+      isDayTime = (now.hour > 6 && now.hour < 21) ? true : false;
       time = DateFormat.jm().format(now);
     } catch (e) {
 
