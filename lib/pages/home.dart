b520952a9e015d5101ad7a193ea730dc4accdb9d
Abraham Ortega <Abraham12orca@gmail.com>
2022-06-08 12:49:33 +0200

    Assets added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,40 +17,45 @@ class _HomeState extends State<Home> {
     data =  ModalRoute.of(context)!.settings.arguments as Map;
     print(data);
 
+    // set background
+    String bgImage = data['isDaytime'] ? 'day.png': 'night.png';
+
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-            child: Column(
-              children: <Widget>[
-                FlatButton.icon(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/location');
-                    },
-                    icon: const Icon(Icons.edit_location),
-                    label: const Text('Edit Location'),
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      data['location'],
-                      style: const TextStyle(
-                        fontSize: 28,
-                        letterSpacing: 2,
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  data['time'],
-                  style: const TextStyle(
-                    fontSize: 66
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+              child: Column(
+                children: <Widget>[
+                  FlatButton.icon(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/location');
+                      },
+                      icon: const Icon(Icons.edit_location),
+                      label: const Text('Edit Location'),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        data['location'],
+                        style: const TextStyle(
+                          fontSize: 28,
+                          letterSpacing: 2,
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    data['time'],
+                    style: const TextStyle(
+                      fontSize: 66
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
       ),
